027ab2a6bd8fadbe9e6a6a82c565672ca87df866
Milad Sarabi <miladsarabi786@gmail.com>
2026-02-17 02:03:22 +0430

    Implement dark mode toggle, menu toggler, and active link highlighting

--- js/main.js
+++ js/main.js
@@ -1,1 +1,35 @@
-console.log('hello dear')
+const body = document.body;
+let isDark = body.classList.contains('dark-mode');
+
+document.querySelector('#dark-mode').addEventListener('click', function() {
+    if(isDark) {
+        body.classList.replace('dark-mode', 'light-mode');
+        isDark = false;
+        this.innerText = '🌙';
+    }
+    else {
+        body.classList.replace('light-mode', 'dark-mode');
+        isDark = true;
+        this.innerText = '☀️';
+    }
+})
+
+// Menu Toggler 
+const navMenu = document.querySelector('.nav .list--vertical');
+
+document.getElementById('navMenuToggler').addEventListener('click', function() {
+    navMenu.classList.toggle('collapsible--active');
+})
+
+// Nav bar link
+const navLinks = document.querySelectorAll('.nav li');
+navLinks.forEach(link => {
+    link.addEventListener('click', (e) => {
+        e.preventDefault();
+        const clickedItem = e.target.closest('li');
+        if(clickedItem) {
+            navMenu.querySelector('.link-active')?.classList.remove('link-active');
+            clickedItem.classList.add('link-active');
+        }
+    })
+});
